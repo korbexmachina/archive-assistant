88bfab450b5a06b572cd8273a68308fd9218178a
Korben Tompkin <86434527+korbexmachina@users.noreply.github.com>
2023-07-20 15:56:50 -0700

    added support for setting the file type

--- archive_assistant.py
+++ archive_assistant.py
@@ -8,11 +8,11 @@ import time
 
 def move(type, date, archive_path) -> None:
     try:
-        shutil.move(f"./{type}-{date}.tar.gz", archive_path)
-        print(f"Archive created! {type}-{date}.tar.gz")
+        shutil.move(f"./{type}-{date}{ext}", archive_path)
+        print(f"Archive created! {type}-{date}{ext}")
     except Exception:
-        os.remove(f"./{type}-{date}.tar.gz")
-        print(f"Already created an archive for today, try again tomorrow!\n{type}-{date}.tar.gz")
+        os.remove(f"./{type}-{date}{ext}")
+        print(f"Already created an archive for today, try again tomorrow!\n{type}-{date}{ext}")
     return
 
 def cleanup(type, archive_path) -> None:
@@ -22,9 +22,34 @@ def cleanup(type, archive_path) -> None:
             death = os.path.join(archive_path, i)
             os.remove(death)
 
+def file_type(option) -> str:
+    global ext
+    if option == 0:
+        ext = ".zip"
+        return "zip"
+    elif option == 1:
+        ext = ".tar"
+        return "tar"
+    elif option == 2:
+        ext = ".tar.gz"
+        return "gztar"
+    elif option == 3:
+        ext = ".tar.bz2"
+        return "bztar"
+    elif option == 4:
+        ext = ".tar.xz"
+        return "xztar"
+    else:
+        print("Invlid archive option! Defaulting to zip")
+        ext = ".zip"
+        return "zip"
+
 def main():
 
-    CONST_PATH = os.path.expanduser("~/.config/archive-assistant/archivePaths.txt") # The path to the config file that contains the paths
+    # CONST_PATH = os.path.expanduser("~/.config/archive-assistant/archivePaths.txt")
+    # The path to the config file that contains the paths
+
+    CONST_PATH = os.path.expanduser("./archivePaths.txt") # Path for testing
 
     if not os.path.exists(CONST_PATH):
         os.mkdir(CONST_PATH)
@@ -33,12 +58,15 @@ def main():
     with open(CONST_PATH,'r') as sys.stdin:
         vault_path = os.path.expanduser(input()) # PATH TO VAULT
         archive_path = os.path.expanduser(input()) # PATH TO BACKUP DIRECTORY
+        archive_option = int(input()) # TYPE OF ARCHIVE
 
     if not os.path.exists(vault_path):
         os.mkdir(vault_path)
 
     if not os.path.exists(archive_path):
         os.mkdir(archive_path)
+
+    archive_format = file_type(archive_option)
 
     daily = 0
     monthly = 0
@@ -61,7 +89,7 @@ def main():
     else: # Otherwise create a daily backup
         type = "daily-archive"
 
-    shutil.make_archive(f"{type}-{date}", "gztar", vault_path)
+    shutil.make_archive(f"{type}-{date}", archive_format, vault_path)
     move(type, date, archive_path)
 
     if clean != "":
